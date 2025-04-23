79fa54a591fd98083d4a921070b3ab2416bd5d5c
Swaggyxren <92450004+Swaggyxren@users.noreply.github.com>
2025-04-23 13:37:15 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = 'b981f276228a9cfc85b752bfc49d54d9';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
@@ -14,7 +14,7 @@ const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
 
   // Fetch from multiple pages to get more anime (max 3 pages for demo)
   for (let page = 1; page <= 3; page++) {
-    const res = await fetch(`${BASE_URL}/trending/tv/week?api_key=${API_KEY}&page=${page}`);
+    const res = await fetch(`${BASE_URL}/trending/tv/week?_key=${_KEY}&page=${page}`);
     const data = await res.json();
     const filtered = data.results.filter(item =>
       item.original_language === 'ja' && item.genre_ids.includes(16)
